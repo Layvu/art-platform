ecd40b06583bdc05a3a6c0a022cfd4e4f6603d9a
koborochka <vitaliykorobov103@gmail.com>
2025-11-20 16:34:15 +0500

    feat(skeleton): пример использования skeleton на products/loading

--- src/app/(public)/products/loading.tsx
+++ src/app/(public)/products/loading.tsx
@@ -1,5 +1,19 @@
+import { Skeleton } from "@/components/ui/skeleton";
+
 export default function Loading() {
   return (
-    <div>loading from app/products/loading</div>
+    <div className="flex items-center space-x-4 flex-col mb-4">
+      <Skeleton className="h-12 w-120 mb-4" />
+      <div className="grid grid-cols-4 space-y-2">
+        <Skeleton className="h-40 w-[250px]" />
+        <Skeleton className="h-40 w-[200px]" />
+        <Skeleton className="h-40 w-[250px]" />
+        <Skeleton className="h-40 w-[200px]" />
+        <Skeleton className="h-40 w-[250px]" />
+        <Skeleton className="h-40 w-[200px]" />
+        <Skeleton className="h-40 w-[250px]" />
+        <Skeleton className="h-40 w-[200px]" />
+      </div>
+    </div>
   )
 }
